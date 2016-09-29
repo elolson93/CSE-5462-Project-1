8f7f85b1ecafbbd8a8e258fbdb0d8fda723ce398
baker1927 <baker.1927@osu.edu>
2016-09-29 11:06:47 -0400

    Added usleep() to fix large file issue, added header, cleaned up comments
    ftpc was sending too many packets to tcpd to handle at a time. Added a usleep(100) to add delay between sends which seemed to remedy the issue.

--- ftpc.c
+++ ftpc.c
@@ -1,133 +1,97 @@
 /*
 / Eric Olson and James Baker
 / CSE 5462 Network Programming
-/ Lab 3 - September 15, 2016
+/ Project 1 - Checkpoint 1 - September 29, 2016
 / 
-/ This file contains our implementation of a TCP client.
+/ This file contains our implementations of a TCP server.
 */
 
+#include "header.h"
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
-#include <string.h>
-#define MSS 1000
-#define LOCALPORT 9999
-#define LOCALADDRESS "127.0.0.1"
-
-/* client program called with host name where server is run */
-int main(int argc, char *argv[]) {
-	/* validate input args */
-	if(argc < 1) {
-		fprintf(stderr, "Error: Include file name is arguments.\n");
-		exit(1);
-	}
-
-	int sock;                     /* initial socket descriptor */
-	int rval;                     /* returned value from a read */
-	int num_bytes;		      /* number of bytes of file to be sent */
-	int num_read;		      /* bytes read from file stream */
-	struct sockaddr_in sin_addr;  /* structure for socket name setup */
-	char * file_name = argv[1];   /* file name */
-	char buf[MSS] = {0};          /* bytes to send to server */
-	struct hostent *hp;	      /* host */
+int main(int argc, char* argv[]) {
 	
-	printf("%s\n\n", "TCP client preparing to send file...");
+    	int sock;                     /* initial socket descriptor */
+    	struct sockaddr_in sin_addr; /* structure for socket name setup */
 
-	/* initialize socket connection in unix domain */
-	if((sock = SOCKET(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("Error opening socket");
-		exit(1);
-	}
 
-  	/* construct name of socket to send to */
-  	sin_addr.sin_family = AF_INET;
-  	sin_addr.sin_port = 0; /* fixed by adding htons() */
-	sin_addr.sin_addr.s_addr = INADDR_ANY;
+    	char fileName[20] = {'\0'};
+    	int fileSize = 0;
+    	char readBuffer[MSS] = {0};
+	struct stat st = {0};
+	char pathName[] = "recvd/";
+    	printf("TCP server waiting for remote connection from clients ...\n\n");
+ 
+    	/*initialize socket connection in unix domain*/
+    	if((sock = SOCKET(AF_INET, SOCK_STREAM, 0)) < 0){
+    		perror("Error opening socket");
+    		exit(1);
+    	}
+    	sin_addr.sin_family = AF_INET;
+    	sin_addr.sin_port = htons(LOCALPORT);
+    	sin_addr.sin_addr.s_addr = INADDR_ANY;
+
+    	/* bind socket name to socket */
+    	if(BIND(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
+      		perror("Error binding stream socket");
+      		exit(1);
+    	}
   
-  	/* establish connection with server */
-  	if(CONNECT(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) 
-	{
-    		close(sock);
+  	/* accept a connection in socket msgsocket */ 
+  	if((ACCEPT(sock, (struct sockaddr *)NULL, (socklen_t *)NULL)) == -1) { 
     		perror("Error connecting stream socket");
     		exit(1);
   	}
-  
-  	/* Open file for transfer */
-  	FILE *fp = fopen(file_name,"rb");
-  	if(fp==NULL) 
-	{
-    		perror("Error opening file");
-    		exit(1);  
+  	/* get the size of the payload */
+  	if (RECV(sock, &fileSize, 4, 0, NULL, NULL) < 4) {
+  		printf("%s\n", "Error: The size read returned less than 4");
+  		exit(1);
   	}
+	printf("Recieved size: %d bytes\n\n", fileSize);
 
-  	/* Read number of bytes in file, rewind file to start index */  
-	fseek(fp, 0L, SEEK_END);
-	num_bytes = ftell(fp);
-	rewind(fp);
-        
-	/* Send file size in 4 bytes */
-	SEND(sock, &num_bytes, MSS, 0);
-	printf("Sent size: %i bytes\n\n", num_bytes);
-	//sleep(0.1);
-	
+  	/* get the name of the file */
+  	if (RECV(sock, fileName, sizeof(fileName), 0) < 20) {
+  		printf("%s\n", "Error: The name read returned less than 20");
+  		exit(1);
+  	}
+	printf("Received name: %s\n\n", fileName);
 
-	/* Send file name in 20 bytes */
-	SEND(sock, file_name, MSS, 0);
-	printf("Sent name: %s\n\n", file_name);
-	//sleep(0.1);
-
-	//printf("Size real: %i", num_bytes);
-	//char num_str[4];
-	//sprintf(num_str, "%d", num_bytes);
-	//printf("Size of %s: %zu",num_str, sizeof(num_str));
-
-	//printf("Size of name: %i", sizeof(file_name));
-	bzero(buf, sizeof(buf));
-	int num_sent_total = 0;
-	int num_sent = 0;
-	while((num_read = fread(buf,1,sizeof(buf), fp)) > 0)
-	{
-		
-		//printf("Read in: \n%s\n", buf);
-	  	/* Read file in chunks of 512 bytes */
-		//num_read = fread(buf,1,MSS,fp);
-
-		/* If read was successful send data. */
-		/*if(num_read > 0)
-		{*/
-		num_sent = SEND(sock, buf, num_read, 0);
-		num_sent_total += num_sent;
-		//printf("Bytes Sent: %i\n", num_sent_total); 
-		//printf("Sent message:\n%s\n\n", buf);
-		//sleep(0.0000001);
-		/*}*/
-
-		/* Handle end of file or read error */
-		/*if (num_read < MSS)
-		{
-		  	if (feof(fp)) 
-		  	{
-		  		break;
-		  	}
-		  	else if (ferror(fp))
-		  	{
-				perror("Error reading file\n");
-				exit(1);
-		  	}
-		}
-		bzero(buf, sizeof(buf));*/
+  	/* create a directory if one does not already exist */  
+	if (stat("recvd", &st) == -1) {
+    		mkdir("recvd", 0700);
 	}
 
-	printf("%s\n", "File Sent.");
+  	FILE* output = fopen(strcat(pathName, fileName), "wb");
+	
+  	if (NULL == output) {
+		fprintf(stderr, "%s\n", "Error opening the output file");
+		exit(1);
+	}
 
-	/* Close file and connection */
-	close(fp);
-	close(sock);
-	return(0); 
+  	/* read the payload from the stream until the whole payload has been read */
+  	int amtReadTotal = 0;
+  	int amtRead = 0;
+  	while (amtReadTotal < fileSize) { 
+		/* Receive from server side tcpd */
+  		amtRead = RECV(sock, readBuffer, sizeof(readBuffer), MSG_WAITALL);
+		
+		/* track byes recieved */
+  		amtReadTotal += amtRead;
+  		if (amtRead < 0) {
+  			fprintf(stderr, "%s\n\n", "Error reading from the connection stream. Server terminating");
+  			exit(1);
+  		} 
+
+  		/* write the received data to the output file */
+  		fwrite(readBuffer, 1, amtRead, output);
+  	}
+
+	printf("Recieved file.\n");
+  
+  	/* close the output file and connections */
+  	close(sock);
+  	fclose(output);
+
+  	return 0;
 }
+
+
