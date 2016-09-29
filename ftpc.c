89c7d19faa8d833d0f5962ffc8bbe3df90524320
baker1927 <baker.1927@osu.edu>
2016-09-29 11:08:00 -0400

    Added usleep() to fix large file issue, added header, cleaned up comments

--- ftpc.c
+++ ftpc.c
@@ -3,95 +3,91 @@
 / CSE 5462 Network Programming
 / Project 1 - Checkpoint 1 - September 29, 2016
 / 
-/ This file contains our implementations of a TCP server.
+/ This file contains our implementation of a TCP client.
 */
 
 #include "header.h"
 
-int main(int argc, char* argv[]) {
-	
-    	int sock;                     /* initial socket descriptor */
-    	struct sockaddr_in sin_addr; /* structure for socket name setup */
-
-
-    	char fileName[20] = {'\0'};
-    	int fileSize = 0;
-    	char readBuffer[MSS] = {0};
-	struct stat st = {0};
-	char pathName[] = "recvd/";
-    	printf("TCP server waiting for remote connection from clients ...\n\n");
- 
-    	/*initialize socket connection in unix domain*/
-    	if((sock = SOCKET(AF_INET, SOCK_STREAM, 0)) < 0){
-    		perror("Error opening socket");
-    		exit(1);
-    	}
-    	sin_addr.sin_family = AF_INET;
-    	sin_addr.sin_port = htons(LOCALPORT);
-    	sin_addr.sin_addr.s_addr = INADDR_ANY;
-
-    	/* bind socket name to socket */
-    	if(BIND(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
-      		perror("Error binding stream socket");
-      		exit(1);
-    	}
-  
-  	/* accept a connection in socket msgsocket */ 
-  	if((ACCEPT(sock, (struct sockaddr *)NULL, (socklen_t *)NULL)) == -1) { 
-    		perror("Error connecting stream socket");
-    		exit(1);
-  	}
-  	/* get the size of the payload */
-  	if (RECV(sock, &fileSize, 4, 0, NULL, NULL) < 4) {
-  		printf("%s\n", "Error: The size read returned less than 4");
-  		exit(1);
-  	}
-	printf("Recieved size: %d bytes\n\n", fileSize);
-
-  	/* get the name of the file */
-  	if (RECV(sock, fileName, sizeof(fileName), 0) < 20) {
-  		printf("%s\n", "Error: The name read returned less than 20");
-  		exit(1);
-  	}
-	printf("Received name: %s\n\n", fileName);
-
-  	/* create a directory if one does not already exist */  
-	if (stat("recvd", &st) == -1) {
-    		mkdir("recvd", 0700);
-	}
-
-  	FILE* output = fopen(strcat(pathName, fileName), "wb");
-	
-  	if (NULL == output) {
-		fprintf(stderr, "%s\n", "Error opening the output file");
+/* client program called with file name and no args*/
+int main(int argc, char *argv[]) {
+	/* validate input args */
+	if(argc < 1) {
+		fprintf(stderr, "Error: Include file name is arguments.\n");
 		exit(1);
 	}
 
-  	/* read the payload from the stream until the whole payload has been read */
-  	int amtReadTotal = 0;
-  	int amtRead = 0;
-  	while (amtReadTotal < fileSize) { 
-		/* Receive from server side tcpd */
-  		amtRead = RECV(sock, readBuffer, sizeof(readBuffer), MSG_WAITALL);
-		
-		/* track byes recieved */
-  		amtReadTotal += amtRead;
-  		if (amtRead < 0) {
-  			fprintf(stderr, "%s\n\n", "Error reading from the connection stream. Server terminating");
-  			exit(1);
-  		} 
+	int sock;                     /* initial socket descriptor */
+	int rval;                     /* returned value from a read */
+	int num_bytes;		      /* number of bytes of file to be sent */
+	int num_read;		      /* bytes read from file stream */
+	int num_sent_total = 0;
+	int num_sent = 0;
+	struct sockaddr_in sin_addr;  /* structure for socket name setup */
+	char * file_name = argv[1];   /* file name */
+	char buf[MSS] = {0};          /* bytes to send to server */
+	struct hostent *hp;	      /* host */
+	
+	printf("%s\n\n", "TCP client preparing to send file...");
 
-  		/* write the received data to the output file */
-  		fwrite(readBuffer, 1, amtRead, output);
+	/* initialize socket connection in unix domain */
+	if((sock = SOCKET(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Error opening socket");
+		exit(1);
+	}
+
+  	/* construct name of socket to send to */
+  	sin_addr.sin_family = AF_INET;
+  	sin_addr.sin_port = 0; /* Kernel assign*/
+	sin_addr.sin_addr.s_addr = INADDR_ANY; /* Kernel assign */
+  
+  	/* establish connection with server */
+  	if(CONNECT(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) 
+	{
+    		close(sock);
+    		perror("Error connecting stream socket");
+    		exit(1);
+  	}
+  
+  	/* Open file for transfer */
+  	FILE *fp = fopen(file_name,"rb");
+  	if(fp==NULL) 
+	{
+    		perror("Error opening file");
+    		exit(1);  
   	}
 
-	printf("Recieved file.\n");
-  
-  	/* close the output file and connections */
-  	close(sock);
-  	fclose(output);
+  	/* Read number of bytes in file, rewind file to start index */  
+	fseek(fp, 0L, SEEK_END);
+	num_bytes = ftell(fp);
+	rewind(fp);
+        
+	/* Send file size in 4 bytes */
+	SEND(sock, &num_bytes, MSS, 0);
+	printf("Sent size: %i bytes\n\n", num_bytes);
 
-  	return 0;
+	/* Send file name in 20 bytes */
+	SEND(sock, file_name, MSS, 0);
+	printf("Sent name: %s\n\n", file_name);
+
+	bzero(buf, sizeof(buf));
+
+	while((num_read = fread(buf,1,sizeof(buf), fp)) > 0)
+	{
+		/* send buffer client side tcpd */
+		num_sent = SEND(sock, buf, num_read, 0);
+
+		/* for bookkeeping */
+		num_sent_total += num_sent;
+
+		/* added to avoid overloading client side tcpd when sending large files */
+		usleep(1000);
+	}
+
+	printf("%s\n", "File Sent.");
+
+	/* Close file and connection */
+	close(fp);
+	close(sock);
+	return(0); 
 }
-
-
