6a1d8b8d6c7584f1e30ecd959dfcfd22ec44bef7
baker1927 <baker.1927@osu.edu>
2016-09-29 11:04:54 -0400

    Changed to use header, tweaks, comments.

--- capitalFunctions.c
+++ capitalFunctions.c
@@ -1,31 +1,12 @@
 /*
 / Eric Olson and James Baker
 / CSE 5462 Network Programming
-/ Lab 3 - September 15, 2016
+/ Project 1 - Checkpoint 1 - September 29, 2016
 / 
 / This file contains our implementations of TCP functions using UDP.
 */
-#include <stdio.h>
-#include <strings.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#define LOCALPORT 9999
-#define LOCALADDRESS "127.0.0.1"
-#define MSS 1000
 
-
-typedef struct tcpdHeader {
-	int flag;
-	size_t maxData;
-	char body[MSS];
-	int bytes_to_read;
-} tcpdHeader;
-
-tcpdHeader clientHeader;
+#include "header.h"
 
 /* Prototypes for each function */ 
 ssize_t SEND(int socket, const void* buffer, size_t length, int flags);
@@ -36,10 +17,9 @@ int CONNECT(int sockfd, const struct sockaddr *addr,
                    socklen_t addrlen);
 int SOCKET(int domain, int type, int protocol);
 
-
-
+/* Function implmentations */
 int ACCEPT(int sock, struct sockaddr *address, socklen_t *address_len) {
-	return sock;
+	return 0;
 }
 
 int CONNECT(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
@@ -54,55 +34,35 @@ int BIND(int socket, struct sockaddr *my_addr, socklen_t addrlen) {
         return bind(socket, (struct sockaddr *)my_addr, addrlen);
 }
 
-	
-
 ssize_t SEND(int socket, const void* buffer, size_t length, int flags) {
+
 	/* define address of local socket to pass data to */
 	struct sockaddr_in daemon_addr;
 	daemon_addr.sin_family = AF_INET;
 	daemon_addr.sin_port = htons(LOCALPORT);
 	daemon_addr.sin_addr.s_addr = INADDR_ANY;
 
+	/* wrapper for sending data to tcpd */
+	tcpdHeader clientHeader;
 
+	/* indicates packet is from client process */
+	/* client = 1/server = 0 */
 	clientHeader.flag = 1;
+
+	/* Passes along the amount of actual bytes to be read from payload */
+	/* IMPORTANT for only for first two and last packet sent */
 	clientHeader.bytes_to_read = length;
+
+	/* Copy buffer contents to payload to tcpd */
 	bcopy(buffer,clientHeader.body, length);
 
-	//printf("Body sent: %s\n", clientHeader.body);
-	//sleep(0.00001);
-	/* pass data to local socket in tcpd */
-	ssize_t sent = 0;
-	sent = sendto(socket, (char *)&clientHeader, sizeof(clientHeader), flags, 
+	/* Send packet to client side tcpd */
+	return sendto(socket, (char *)&clientHeader, sizeof(clientHeader), flags, 
 		(struct sockaddr *)&daemon_addr, sizeof(daemon_addr));
-	
-	//free(*clientHeader);
-	//nanosleep(0.1);
-	return sent;
 }
 
 ssize_t RECV(int socket, void* buffer, size_t length, int flags) {
 
-	/*int troll_sock;
-	if((troll_sock = SOCKET(AF_INET, SOCK_DGRAM, 0)) < 0)
-	{
-		perror("Error opening socket");
-		exit(1);
-	}	
-	struct sockaddr_in daemon_addr;
-	daemon_addr.sin_family = AF_INET;
-	daemon_addr.sin_port = htons(LOCALPORT);
-	daemon_addr.sin_addr.s_addr = INADDR_ANY;
-	
-	tcpdHeader serverHeader;
-	serverHeader.flag = 0;
-	serverHeader.maxData = length;
-	
-	sendto(troll_sock, (char *)&serverHeader, sizeof(serverHeader), 0, (struct sockaddr *)&daemon_addr, sizeof(daemon_addr));*/
-	
-
-	/* RECEIVE DATA */
-	size_t temp;
-	temp = recvfrom(socket, buffer, length, 0, NULL, NULL);
-	nanosleep(10000);
-	return temp;
+	/* receive data from tcpd */
+	return recvfrom(socket, buffer, length, flags, NULL, NULL);
 }
